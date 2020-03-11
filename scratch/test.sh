7509858eeeaad3910840acf8f717b3d3fac28950
Hayden Grimes <haydengrimes1@github.com>
2020-03-11 02:25:42 +0000

    testing

--- scratch/test.sh
+++ scratch/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Testing"
+read -p "What is your name: " name
+echo "hello $name"
 
+read -s -p "What is your password: " pass
+echo  "Password is : $pass"
+
+read -n1 -p "Press and Key to exit"
 exit 0
+
+
