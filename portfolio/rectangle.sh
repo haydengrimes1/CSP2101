7866b8a929949839d77f44cad439b38d102a38d6
Hayden Grimes <haydengrimes1@github.com>
2020-05-15 06:39:31 +0000

    Added Commenting

--- portfolio/rectangle.sh
+++ portfolio/rectangle.sh
@@ -3,9 +3,11 @@
 # Student Name : Hayden Grimes
 # Student Number : 10424698
 
+#Define Global Variables
 fileName='./rectangle.txt'
 destinationFileName="./rectangle_f.txt"
 
+#Function to check that the source file exists
 function checkSourceFile () {
     if [ -f $fileName ]; then
         true
@@ -15,10 +17,13 @@ function checkSourceFile () {
     fi
 }
 
+#Main function that gets executed 
 function homeScreen () {
     selection=9
+    #Until loop to repeat selection until user exits
     until (( $selection == 0 ))
     do
+        #Prints homescreen
         echo -e "\e[96m\nWelcome to the File Format Program.\n"
         echo -e "Current Input File:\t\e[36m" $fileName
         echo -e "\e[96mCurrent Output File:\t\e[36m" $destinationFileName
@@ -27,6 +32,7 @@ function homeScreen () {
         echo -e "\e[96m3: \e[36mChange Output File\n"
         echo -e "\e[96m0: \e[31mExit Program\n\n\e[96m"
         read -p "Please enter your Selection: " selection
+        #Case statement to handle users selection
         case $selection in
             "1")
                 if [ -f $fileName ]; then
@@ -50,49 +56,67 @@ function homeScreen () {
     done
 }
 
+#Function to check the destination file exists
 function checkDestinationFile () {
     if [ -f $destinationFileName ]; then
         echo -e "\e[91m\n$destinationFileName already exists. Removing...\e[39m"
+        # Removes destination file
         rm $destinationFileName
         echo -e "\e[91mRemoved $destinationFileName\e[39m"
     fi
 }
 
+# Function to perform the Sed command
 function sedFile () {
     counter=0
     dataAmount=0
     error=0
+    # While loop reads every line within the source file
     while read -r line
     do
         ((counter++))
+        # Checks if currnet line is the header line
         if [ $counter = 1 ]; then
             IFS=","
             read -ra data <<< $line
+            # Sets dataamount variables to the amount of fields in the header
             dataAmount=${#data[@]}
         else
             IFS=","
+            # Splits the line up into array of each data field
             read -ra data <<< $line
+            # Checks if the amount of fields in a line is the same as the header
             if [ $dataAmount != ${#data[@]} ]; then
                 error=1
                 break
             fi
             string='Rec*'
+            # Checks if first field in line starts with Rec
             if [[ "${data[0]}" != $string ]]; then
                 error=1
                 break
             fi
         fi
     done < "$fileName"
+    # Checks if any errors where made in the check above
     if [ $error = 0 ]; then
         checkDestinationFile
+        # Reads the header of the file to get Field names
         read header < $fileName
         IFS=","
         read -ra header <<< $header
+        # Runs Sed command
+        # Deletes the header line
         sed -e '1d'\
+        # Formats the first Field (Name field)
             -e 's/Rec/'${header[0]}': Rec/'\
+        # Formats the second Field
             -e 's/,/\t\t'${header[1]}': /'\
+        # Formats the third Field
             -e 's/,/\t\t'${header[2]}': /'\
+        # Formats the fourth Field
             -e 's/,/\t\t'${header[3]}': /'\
+        # Formats the fifth Field and defines source and destination file
             -e 's/,/\t\t'${header[4]}': /' $fileName > $destinationFileName
         echo -e "\n\e[96m'$fileName' has been Formatted and saved at '$destinationFileName'"
     else
@@ -100,16 +124,21 @@ function sedFile () {
     fi
     
 }
-
+# Function to change the source file
 function changeSourceFile () {
     selection2=""
+    # Until loop to ensure users doesnt specify anything other then y or n
     until [ "$selection2" == "y" ] || [ "$selection2" == "n" ]
         do
+        # Prints the current file name
         echo -e "\e[96m\nThe Current Input File is '" $fileName "'. Is this correct? (y/n)" 
+        # Prompts for input y or n
         read -p "Input 'y' or 'n': " selection2
         if [ $selection2 == "n" ]; then
+        # Prompts for input of new file name
             read -p "Please specify a new File: " answer
             if [ -f "$answer" ]; then
+            # Sets Global variable to new input
                 fileName=$answer
             else
                 echo -e "\e[31m\nError: That file doesn't exist\e[39m"
@@ -124,13 +153,18 @@ function changeSourceFile () {
     done
 }
 
+# Function to change the Destination file
 function changeDestinationFile () {
     selection2=""
+    # Until loop to ensure users doesnt specify anything other then y or n
     until [ "$selection2" == "y" ] || [ "$selection2" == "n" ]
         do
+        # Prints the current file name
         echo -e "\e[96m\nThe Current Output file is '" $destinationFileName "'. Is this correct? (y/n)" 
+        # Prompts for input y or n
         read -p "Input 'y' or 'n': " selection2
         if [ $selection2 == "n" ]; then
+        # Prompts for input of new file name and sets answer to the global variable
             read -p "Please specify a new File: " destinationFileName
         elif [ $selection2 == "y" ]; then
             selection2="y"
@@ -140,7 +174,7 @@ function changeDestinationFile () {
         fi
     done
 }
-
+# Runs the home screen function 
 homeScreen
 
 exit 0
--- scratch/test.sh
+++ scratch/test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-net_info="$(ifconfig)"
-address="$(echo "$net_info" | grep inet)"
-
-echo -e "IP addresses on this compputer are:\n$address"
+echo "Hello"
